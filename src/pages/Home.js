29f0e5e675b7169909cd8bfc83b820f25d2d7be1
joselu <jose.lu.jlsz@gmail.com>
2024-06-18 00:17:07 -0400

    se añadio boceto de la pagina de login

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginContainer from '../components/LoginContainer'
+
+const Home = () => {
+  return (
+    <body className='h-full flex justify-center bg-red-100'>
+      <LoginContainer />
+    </body>
+  )
+}
+
+export default Home
